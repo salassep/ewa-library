1e090b6595eb10a245bf56c7bf3fec6f70ae9710
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2023-07-03 10:02:38 +0800

    create library project

---
+++ example/ewa_library_example.dart
@@ -0,0 +1,6 @@
+import 'package:ewa_library/ewa_library.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/ewa_library.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/ewa_library_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
