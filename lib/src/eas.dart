05d9041d6a432955e8fcfce015ed9a9b5f564739
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2023-07-06 22:51:25 +0800

    Refactor code for improved readability and add documentation in eas.dart

--- lib/src/eas.dart
+++ lib/src/eas.dart
@@ -1,69 +1,82 @@
-import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
-import 'separator.dart';
 
 class Eas {
-  Future<List<int>> embed(String data) async {
-    final file = await File('test_data/sample5.wav').readAsBytes();
 
-    // change fileBytes from uint8list to list<int>, so it can be modified
-    final List<int> fileBytesInt = List<int>.from(file);
-
-    List<int> embeddableBitIndex = <int>[];
-
-    for (var i = 0; i < fileBytesInt.length; i++) {
-      if (fileBytesInt[i] >= 254) {
-        embeddableBitIndex.add(i);
+  /// To obtain the index of selective bytes only (254 and 255) 
+  List<int> _getSelectiveBytesIndex(List bytes) {
+    final List<int> selectiveByteIndex = <int>[];
+    // Selective bytes are 254 and 255
+    for (var i = 0; i < bytes.length; i++) {
+      if (bytes[i] >= 254) {
+        selectiveByteIndex.add(i);
       }
     }
 
-    if (embeddableBitIndex.length < data.length) throw Exception('song isnt have enough room');
+    return selectiveByteIndex;
+  }
+  
+  /// To obtain the audio capacity that will be used for the cover
+  /// and return the amount of capacity in bits.
+  Future<int> getAudioCapacity(File audioCover) async{
+    final Uint8List audioCoverBytes = await audioCover.readAsBytes();
+    final List<int> selectiveBytesIndex = _getSelectiveBytesIndex(audioCoverBytes);
 
-    // final countBit = embeddableBitIndex.length;
-    // final countByte = countBit/8;
-    // final countKiloByte = countByte / 1024;
-
-    // print('kapasitas: $countBit');
-    // print('kapasitas per bytes: $countByte');
-    // print('kapasitar per kb: $countKiloByte');
-
-    for (var i = 0; i < data.length; i++) {
-      fileBytesInt[embeddableBitIndex[i]] = data[i] == '1' ? 255 : 254;
-    }
-
-    return fileBytesInt;
+    return selectiveBytesIndex.length;    
   }
 
-  Future<List<int>> extract() async {
-    final file = await File('test_data/result/stego3.wav').readAsBytes();
+  /// Embed data to audio cover
+  Future<List<int>> embed({required File audioCover, required String dataToHide}) async {
+    final Uint8List audioCoverBytes = await audioCover.readAsBytes();
 
-    // change fileBytes from uint8list to list<int>, so it can be modified~
-    final List<int> fileBytesInt = List<int>.from(file);
+    // change fileBytes from uint8list to list<int>, so it can be modified
+    final List<int> convertedAudioCoverBytes = List<int>.from(audioCoverBytes);
 
-    // Create an array consisting of 8 characters per element
-    List<String> embeddableBits = <String>[];
-    for (var i = 0; i < fileBytesInt.length; i++) {
-      if (embeddableBits.isNotEmpty && embeddableBits[embeddableBits.length - 1].length < 8) {
-        if (fileBytesInt[i] == 254) {
-          embeddableBits[embeddableBits.length - 1] += '0';
-        } else if (fileBytesInt[i] == 255) {
-          embeddableBits[embeddableBits.length - 1] += '1';
+    List<int> selectiveBytesIndex = _getSelectiveBytesIndex(convertedAudioCoverBytes);
+
+    if (selectiveBytesIndex.length < dataToHide.length) throw Exception('Audio doesn\'t have enough capacity');
+
+    // Replacing selective byte valkue, 
+    // where if the data is 1, the selective bytes are changed to 255, 
+    // and if the data is 0, the selective bytes are set to 254.
+    for (var i = 0; i < dataToHide.length; i++) {
+      convertedAudioCoverBytes[selectiveBytesIndex[i]] = dataToHide[i] == '1' ? 255 : 254;
+    }
+
+    return convertedAudioCoverBytes;
+  }
+
+  /// Extract data from an audio
+  Future<List<int>> extract(File audioCover) async {
+    final Uint8List audioCoverBytes = await audioCover.readAsBytes();
+
+    // Change fileBytes from uint8list to list<int>, so it can be modified~
+    final List<int> convertedAudioCoverBytes = List<int>.from(audioCoverBytes);
+
+    // Get selective bytes, and create an array from it, consisting of 8 characters per element
+    List<String> selectiveBytes = <String>[];
+    for (var i = 0; i < convertedAudioCoverBytes.length; i++) {
+      if (selectiveBytes.isNotEmpty && selectiveBytes[selectiveBytes.length - 1].length < 8) {
+        if (convertedAudioCoverBytes[i] == 254) {
+          selectiveBytes[selectiveBytes.length - 1] += '0';
+        } else if (convertedAudioCoverBytes[i] == 255) {
+          selectiveBytes[selectiveBytes.length - 1] += '1';
         }
       } else {
-        if (fileBytesInt[i] == 254) {
-          embeddableBits.add('0');
-        } else if (fileBytesInt[i] == 255) {
-          embeddableBits.add('1');
+        if (convertedAudioCoverBytes[i] == 254) {
+          selectiveBytes.add('0');
+        } else if (convertedAudioCoverBytes[i] == 255) {
+          selectiveBytes.add('1');
         }
       }
     }
 
-    final List<int> convertedEmbeddableBits = <int>[];
-    for (var bit in embeddableBits) {
-      convertedEmbeddableBits.add(int.parse(bit, radix: 2));
+    // Convert the selective bytes, which was previously an 8 bit binary string into decimal
+    final List<int> convertedSelectiveBytes = <int>[];
+    for (var bit in selectiveBytes) {
+      convertedSelectiveBytes.add(int.parse(bit, radix: 2));
     }
 
-    return convertedEmbeddableBits;  
+    return convertedSelectiveBytes;  
   }
 }
