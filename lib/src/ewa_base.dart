2b92d24f10c943b5bb29e3276d6889f00e0c91c6
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2023-07-07 23:11:33 +0800

    create class ewa and add documentation in it

--- lib/src/ewa_base.dart
+++ lib/src/ewa_base.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 import 'aes.dart';
@@ -7,69 +6,119 @@ import 'package:path/path.dart';
 import 'utils/separator.dart';
 import 'eas.dart';
 
-void encryptEmbed() async {
-  final file = File('test_data/test.png');
-  final fileExtension = basename(file.path).split('.')[1];
-  final fileBytes = await file.readAsBytes();
+class Ewa {
 
-  // change fileBytes from uint8list to list<int>, so it can be modified like insert extension in it.
-  final List<int> fileBytesInt = List<int>.from(fileBytes);
+  /// Get audio cover capacity in number of bytes.
+  Future<int> getAudioCoverCapacity(File audioCover) async {
+    final Uint8List audioCoverBytes = await audioCover.readAsBytes();
 
-  final fileExtensionSeparator = Separator.getSeparatorWithFileExtensionInBytes(fileExtension);
-
-  final AesCbc aesCbc = AesCbc();
-
-  // Insert extension and separator to file bytes
-  fileBytesInt.insertAll(0, fileExtensionSeparator);
-
-  // Compress data
-  final compressedData = GzipCompression.compress(fileBytesInt);
-  // Encrypt data
-  final encryptedData = aesCbc.encrypt(message: compressedData, secretKey: 'sukasukasukasuka', iv: 'kamukamukamukamu');
-
-  final List<int> convertedEncryptedData = List<int>.from(encryptedData);
-  
-  convertedEncryptedData.addAll(Separator.getSeparatorInBytes());
-
-  List<String> paddedData = <String>[];
-  for (var i in convertedEncryptedData) {
-    paddedData.add(i.toRadixString(2).padLeft(8, '0'));
+    return Eas().getAudioCapacity(audioCoverBytes);
   }
 
-  final paddedDataString = paddedData.join('');
+  /// Check data in audio cover, if there is data then return true,
+  /// otherwise return false.
+  Future<bool> checkDataInAudioCover(File audioCover) async {
+    final Uint8List audioCoverBytes = await audioCover.readAsBytes();
+    final List<int> extractedBytes = Eas().extract(audioCoverBytes);
+    final int separatorIndex = Separator.getSeparatorIndex(extractedBytes);
 
-  final Eas eas = Eas();
-  final embeddedSong = await eas.embed(paddedDataString);
+    return separatorIndex >= 0;
+  }
 
-  File('test_data/result/stego3.wav').writeAsBytes(embeddedSong);
-}
+  /// Encrypt the data and then embed it into the audio cover.
+  Future<List<int>> encryptEmbed({
+    required File secretFile,
+    required File audioCover, 
+    required String secretKey,
+    required String iv,
+    required String targetPath,
+  }) async {
+    final AesCbc aesCbc = AesCbc();
+    final Eas eas = Eas();
+    final String secretFileExtension = basename(secretFile.path).split('.').last;
+    final Uint8List secretFileBytes = await secretFile.readAsBytes();
 
-void extractDecrypt() async {
-  final Eas eas = Eas();
-  final AesCbc aesCbc = AesCbc();
-  final extractedData = await eas.extract();
+    // Audio cover must be WAV file, because WAV provides a larger storage capacity
+    // compared to other formats.
+    if (basename(audioCover.path).split('.').last != 'wav') throw Exception('Audio cover must be WAV file');
 
-  final separatorIndex = getSeparatorIndex(extractedData, Separator.getSeparatorInBytes());
+    final Uint8List audioCoverBytes = await audioCover.readAsBytes();
 
-  final encryptedDataAfterExtract = Uint8List.fromList(extractedData.sublist(0, separatorIndex));
+    // change fileBytes from uint8list to list<int>, so it can be modified.
+    final List<int> convertedSecretFileBytes = List<int>.from(secretFileBytes);
 
-  final decryptedData = aesCbc.decrypt(cipher: encryptedDataAfterExtract, secretKey: 'sukasukasukasuka', iv: 'kamukamukamukamu');
-  final decompressedData = GzipCompression.decompress(decryptedData);
+    final List<int> fileExtensionSeparatorBytes = Separator.getSeparatorWithFileExtensionInBytes(secretFileExtension);
 
-  final decodedData = utf8.decode(decompressedData, allowMalformed: true);
-  final fileExtensionAfterProcess = Separator.getFileExtension(decodedData);
+    // Insert extension and separator to file bytes.
+    convertedSecretFileBytes.insertAll(0, fileExtensionSeparatorBytes);
 
-  File('test_data/result/result3.$fileExtensionAfterProcess')
-    .writeAsBytes(decompressedData.sublist(Separator.getSeparatorIndex(decodedData) + Separator.separatorWord.length));
-}
+    // Compress data.
+    final List<int> compressedData = GzipCompression.compress(convertedSecretFileBytes);
+    
+    // Encrypt data.
+    final Uint8List encryptedData = aesCbc.encrypt(message: compressedData, secretKey: secretKey, iv: iv);
 
-int getSeparatorIndex() {
- // Return -1 if the separatorList is not found in data.
-}
+    final List<int> convertedEncryptedData = List<int>.from(encryptedData);
 
+    // Insert bytes separator.
+    convertedEncryptedData.addAll(Separator.getSeparatorInBytes());
 
+    // To convert data into 8 bit binary.
+    List<String> paddedData = <String>[];
+    for (var i in convertedEncryptedData) {
+      paddedData.add(i.toRadixString(2).padLeft(8, '0'));
+    }
 
-void main() async {
-  // encryptEmbed();
-  extractDecrypt();
+    // Change data 8 bit binary to one string.
+    final String paddedDataString = paddedData.join('');
+
+    // Embed data to audio cover.
+    final List<int> embeddedSong = eas.embed(audioCoverBytes: audioCoverBytes , dataToHide: paddedDataString);
+
+    // Crete file with the specified path and the name formatted as WAV
+    await File('${targetPath}stego.wav').writeAsBytes(Separator.separateBytesDataFromExtension(embeddedSong));
+
+    return embeddedSong;
+  }
+
+  /// Extract data form audio cover, and decrypt it
+  Future<List<int>> extractDecrypt({
+    required File audioCover, 
+    required String secretKey,
+    required String iv,
+    required String targetPath,
+  }) async {
+    final Eas eas = Eas();
+    final AesCbc aesCbc = AesCbc();
+
+    // Audio cover must be WAV file, because WAV provides a larger storage capacity
+    // compared to other formats.
+    if (basename(audioCover.path).split('.').last != 'wav') throw Exception('Audio cover must be wav file');
+
+    final Uint8List audioCoverBytes = await audioCover.readAsBytes();
+
+    // Extract data from audio cover.
+    final List<int> extractedData = eas.extract(audioCoverBytes);
+
+    final int separatorIndex = Separator.getSeparatorIndex(extractedData);
+
+    // If data not found in audio cover, throw an error.
+    if (separatorIndex < 0) throw Exception('Data not found within the audio cover');
+
+    // Separate data bytes from the byes that are not part of the data.
+    final List<int> separatedExtractData = Separator.separateBytesData(extractedData, separatorIndex);
+
+    // Decrypt.
+    final List<int> decryptedData = aesCbc.decrypt(cipher: Uint8List.fromList(separatedExtractData), secretKey: secretKey, iv: iv);
+
+    // Decompress data.
+    final List<int> decompressedData = GzipCompression.decompress(decryptedData);
+
+    final String fileExtensionAfterProcess = Separator.getFileExtension(decompressedData);
+
+    // Crete file with the specified path and the name formatted as WAV
+    await File('${targetPath}result.$fileExtensionAfterProcess').writeAsBytes(Separator.separateBytesDataFromExtension(decompressedData));
+
+    return decompressedData;
+  }
 }
